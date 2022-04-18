07a133b1de25b5f81160373385f2c3a1105a7133
Aloisio Formento Junior <aloisio.formento@NMAC-2932.local>
2022-04-18 11:40:21 -0300

    WebService refatorado swiftlint

--- BeatsIOS/Service/WebService.swift
+++ BeatsIOS/Service/WebService.swift
@@ -11,28 +11,23 @@ protocol WebServiceProtocol {
 }
 
 class WebService: WebServiceProtocol {
-    
     private let session: NetworkSession
     private let decoder: JSONDecoder
-    
     init(urlSession: NetworkSession = URLSession.shared, decoder: JSONDecoder = JSONDecoder()) {
         self.session = urlSession
         self.decoder = decoder
     }
-    
     func getFones(completion: @escaping (Result<[FonesModel], NetworkErrors>) -> Void) {
         guard let url = URL(string: "https://42edf8f6-0a48-4351-a67a-7d0f30a8cb68.mock.pstmn.io/") else {
             completion(.failure(.invalidURL))
             return
         }
-        
         session.executeRequest(with: url) { [weak self] (data, response, error) in
             guard let self = self else { return }
             guard let jsonData = data else {
                 completion(.failure(.emptyResponse))
                 return
             }
-            
             do {
                 let decoded = try self.decoder.decode([FonesModel].self, from: jsonData)
                 completion(.success(decoded))
@@ -42,9 +37,3 @@ class WebService: WebServiceProtocol {
         }
     }
 }
-    
-    
-    
-    
-
-    
