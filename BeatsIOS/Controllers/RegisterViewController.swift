175ec06e6acae92220f43240ad4cc511df414e38
Aloisio Formento Junior <aloisio.formento@NMAC-2932.local>
2022-04-18 11:28:30 -0300

    RegisterViewController refatorado swiftlint

--- BeatsIOS/Controllers/RegisterViewController.swift
+++ BeatsIOS/Controllers/RegisterViewController.swift
@@ -8,10 +8,9 @@
 import Foundation
 import UIKit
 
-
 class RegisterViewController: UIViewController {
     
-    //MARK: - Properties
+    // MARK: - Properties
     
     @IBOutlet weak var nomeLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
@@ -24,21 +23,16 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var pwRepeatErrorLabel: UILabel!
     @IBOutlet weak var alreadyRegisterLabel: UILabel!
     @IBOutlet weak var alreadyRegisterButtonLabel: UIButton!
-    
     private lazy var registerVM: RegisterViewModel = RegisterViewModel(delegate: self)
     
-    //MARK: - LifeCycle
-    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
         resetFormulario()
     }
-    
-    //MARK: - Methods
-    
+    // MARK: - Methods
     private func configureUI() {
-        
         self.navigationController?.navigationBar.barStyle = .black
         configureLabels()
         configureButtons()
@@ -46,181 +40,130 @@ class RegisterViewController: UIViewController {
     }
     
     private func configureLabels() {
-        
         nomeLabel.text = registerVM.nomeText
         nomeLabel.textColor = .white
         nomeLabel.font = UIFont.boldSystemFont(ofSize: 50)
         nomeLabel.textAlignment = .center
-        
         descriptionLabel.text = registerVM.subNomeText
         descriptionLabel.textColor = .white
         descriptionLabel.font = UIFont.boldSystemFont(ofSize: 16)
         descriptionLabel.textAlignment = .center
-        
         alreadyRegisterLabel.text = registerVM.alreadyRegisterLabelText
         alreadyRegisterLabel.textColor = .white
         alreadyRegisterLabel.font = UIFont.boldSystemFont(ofSize: 15)
-        
         userErrorLabel.textColor = .red
         userErrorLabel.font = UIFont.boldSystemFont(ofSize: 13)
         pwErrorLabel.textColor = .red
         pwErrorLabel.font = UIFont.boldSystemFont(ofSize: 13)
         pwRepeatErrorLabel.textColor = .red
         pwRepeatErrorLabel.font = UIFont.boldSystemFont(ofSize: 13)
-        
         let greenAttsSB: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor .green, .font: UIFont.boldSystemFont(ofSize: 15)]
         let attibutedTitleSignin = NSMutableAttributedString(string: registerVM.alreadyRegisterButtonText, attributes: greenAttsSB )
         alreadyRegisterButtonLabel.setAttributedTitle(attibutedTitleSignin, for: .normal)
     }
-    
     private func configureButtons() {
-        
         buyButton.layer.cornerRadius = 20
         buyButton.tintColor = .white
         buyButton.setTitle(registerVM.registerButtonText, for: .normal)
         buyButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
         buyButton.setTitleColor(.white, for: .normal)
     }
-    
     private func configureTextFiedls() {
-        
         userTextField.backgroundColor = UIColor(white: 1, alpha: 0.1)
         userTextField.keyboardAppearance = .dark
         userTextField.textColor = .white
         userTextField.attributedPlaceholder = NSAttributedString(string: registerVM.placeholderUserText, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
-        
-        
         pwTextField.backgroundColor = UIColor(white: 1, alpha: 0.1)
         pwTextField.keyboardAppearance = .dark
         pwTextField.textColor = .white
         pwTextField.attributedPlaceholder = NSAttributedString(string: registerVM.placeholderPwText, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
-        
-        
         pwRepeatTextField.backgroundColor = UIColor(white: 1, alpha: 0.1)
         pwRepeatTextField.keyboardAppearance = .dark
         pwRepeatTextField.textColor = .white
         pwRepeatTextField.attributedPlaceholder = NSAttributedString(string: registerVM.placeholderPwRepeatText, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
     }
-    
     private func resetFormulario() {
         buyButton.isEnabled = false
         buyButton.alpha = 0.5
         userErrorLabel.isHidden = false
         pwErrorLabel.isHidden = false
-        
         userErrorLabel.text = ""
         pwErrorLabel.text = ""
         pwRepeatErrorLabel.text = ""
-        
         userTextField.text = ""
         pwTextField.text = ""
         pwRepeatTextField.text = ""
     }
-    
     private func checkForValideForm() {
-        
         buyButton.isEnabled = userErrorLabel.isHidden && pwErrorLabel.isHidden && pwRepeatErrorLabel.isHidden ?  true : false
         buyButton.alpha = userErrorLabel.isHidden && pwErrorLabel.isHidden && pwRepeatErrorLabel.isHidden ? 1 : 0.5
     }
-    
-    //MARK: - Actions
-    
+    // MARK: - Actions
     @IBAction func userTextFieldChanged(_ sender: Any) {
-        
         guard let userName = userTextField.text else { return }
-        
         if userName.isEmpty {
             userErrorLabel.text = ""
         } else {
             if let errorMessage = registerVM.invalidUsername(userName) {
                 userErrorLabel.text = errorMessage
                 userErrorLabel.isHidden = false
-                
-            } else{
+            } else {
                 userErrorLabel.isHidden = true
             }
         }
-        
         checkForValideForm()
-        
     }
-    
     @IBAction func pwTextFieldChanged(_ sender: Any) {
-        
         guard let password = pwTextField.text else { return }
-        
         if password.isEmpty {
             pwErrorLabel.text = ""
         } else {
             if let errorMessage = registerVM.invalidPw(password) {
                 pwErrorLabel.text = errorMessage
                 pwErrorLabel.isHidden = false
-                
-            } else{
+            } else {
                 pwErrorLabel.isHidden = true
             }
         }
-        
         checkForValideForm()
     }
-    
     @IBAction func pwRepeatTextFieldChanged(_ sender: Any) {
-        
         guard let password = pwTextField.text else { return }
         guard let passwordRp = pwRepeatTextField.text else { return }
-        
         if passwordRp.isEmpty {
             pwRepeatErrorLabel.text = ""
         } else {
             if let errorMessage = registerVM.invalidPw(passwordRp) {
                 pwRepeatErrorLabel.text = errorMessage
                 pwRepeatErrorLabel.isHidden = false
-                
-            } else{
+            } else {
                 pwRepeatErrorLabel.isHidden = true
             }
-            
         }
-        
         if let errorMessageValidation = registerVM.validationUser(pwTextField: password, pwRepeatTextField: passwordRp) {
             pwRepeatErrorLabel.text = errorMessageValidation
             pwRepeatErrorLabel.isHidden = false
-        } else{
+        } else {
             pwRepeatErrorLabel.isHidden = true
         }
-        
-        
-        
         checkForValideForm()
     }
-    
     @IBAction func registerButtonAction(_ sender: Any) {
-        
         guard let userEmail = userTextField.text else { return }
         guard let userPw = pwTextField.text else { return }
-        
         registerVM.setUserDefautls(userEmail: userEmail, userPw: userPw)
         resetFormulario()
-        
     }
-    
     @IBAction func alreadyRegisterButtonAction(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
-        
     }
-        
 }
 
 extension RegisterViewController: RegisterViewModelDelegate {
-   
     func showAlert(title: String, message: String, buttonTitle: String) {
         self.displayMyAlertMessage(title: title, message: message, buttonTitle: buttonTitle)
     }
-    
     func showAlertWithPop(title: String, message: String, buttonTitle: String) {
         self.displayMyAlertMessageWithActionPop(title: title, message: message, buttonTitle: buttonTitle)
     }
 }
-
-    
-    
