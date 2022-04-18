40a1dd4a5a8733e8f4f1c40313c43110a41921c4
Aloisio Formento Junior <aloisio.formento@NMAC-2932.local>
2022-04-18 11:07:46 -0300

    Instalado swiftlint, começando a refatorar com base nas dicas do lint

--- BeatsIOS/Controllers/LoginController.swift
+++ BeatsIOS/Controllers/LoginController.swift
@@ -24,7 +24,7 @@ class LoginController: UIViewController {
     @IBOutlet weak var userErrorLabel: UILabel!
     @IBOutlet weak var pwErrorLabel: UILabel!
     
-    private let LoginVM: LoginsViewModel = LoginsViewModel()
+    private let loginVM: LoginsViewModel = LoginsViewModel()
     
     //MARK: - Lifecycle
     
@@ -53,17 +53,17 @@ class LoginController: UIViewController {
     
     private func configureLabels() {
         
-        nameLabel.text = LoginVM.nomeText
+        nameLabel.text = loginVM.nomeText
         nameLabel.textColor = .white
         nameLabel.font = UIFont.boldSystemFont(ofSize: 50)
         nameLabel.textAlignment = .center
         
-        descriptionLabel.text = LoginVM.subNomeText
+        descriptionLabel.text = loginVM.subNomeText
         descriptionLabel.textColor = .white
         descriptionLabel.font = UIFont.boldSystemFont(ofSize: 16)
         descriptionLabel.textAlignment = .center
         
-        signInLabel.text = LoginVM.signUpText
+        signInLabel.text = loginVM.signUpText
         signInLabel.textColor = .white
         signInLabel.font = UIFont.boldSystemFont(ofSize: 15)
         
@@ -77,12 +77,12 @@ class LoginController: UIViewController {
         
         loginButton.layer.cornerRadius = 20
         loginButton.tintColor = .white
-        loginButton.setTitle(LoginVM.loginButtonText, for: .normal)
+        loginButton.setTitle(loginVM.loginButtonText, for: .normal)
         loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
         loginButton.setTitleColor(.white, for: .normal)
         
         let greenAttsSB: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor .green, .font: UIFont.boldSystemFont(ofSize: 15)]
-        let attibutedTitleSignin = NSMutableAttributedString(string: LoginVM.signUpButtonText, attributes: greenAttsSB )
+        let attibutedTitleSignin = NSMutableAttributedString(string: loginVM.signUpButtonText, attributes: greenAttsSB )
         signInButton.setAttributedTitle(attibutedTitleSignin, for: .normal)
     }
     
@@ -91,13 +91,13 @@ class LoginController: UIViewController {
         userTextField.backgroundColor = UIColor(white: 1, alpha: 0.1)
         userTextField.keyboardAppearance = .dark
         userTextField.textColor = .white
-        userTextField.attributedPlaceholder = NSAttributedString(string: LoginVM.placeholderUserText, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        userTextField.attributedPlaceholder = NSAttributedString(string: loginVM.placeholderUserText, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
         
         
         pwTextField.backgroundColor = UIColor(white: 1, alpha: 0.1)
         pwTextField.keyboardAppearance = .dark
         pwTextField.textColor = .white
-        pwTextField.attributedPlaceholder = NSAttributedString(string: LoginVM.placeholderPwText, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        pwTextField.attributedPlaceholder = NSAttributedString(string: loginVM.placeholderPwText, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
     }
     
     private func resetFormulario() {
@@ -122,7 +122,7 @@ class LoginController: UIViewController {
     
     private func checkIsLoggedIn() {
         
-        if LoginVM.checkIsLoggedIn() {
+        if loginVM.checkIsLoggedIn() {
             self.performSegue(withIdentifier: "loginSegue", sender: self)
         }
     }
